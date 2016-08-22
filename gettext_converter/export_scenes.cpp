4e97683e50aa23a2e9573e41e80f807f9dd1130d
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2016-08-23 02:04:22 +1000

    gettext template (POT) exporters for scenes and variables
    This contains TODOs identified after feeding the exported data through the
    Poedit utility.

---
+++ gettext_converter/export_scenes.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using std::cerr;
+using std::cout;
+#include <string>
+using std::string;
+
+#include "scx.hpp"
+
+int main(void) {
+  SCXFile scxfile;
+
+  const auto sourceFile = string{"../../avking.scx"};
+
+  if (!scxfile.read(sourceFile)) {
+    cerr << "Failed to read source: " << sourceFile << "\n";
+    return 1;
+  }
+
+  /*
+   * PO File format:
+   * https://www.gnu.org/software/gettext/manual/gettext.html#PO-Files
+   */
+
+  cout << "# AVKing Scenario Translation\n";
+  cout << "msgid \"\"\n";
+  cout << "msgstr \"\"\n";
+  cout << "\"Language: ja\\n\"\n";
+  cout << "\"Content-Type: text/plain; charset=utf-8\\n\"\n";
+  cout << "\"Content-Transfer-Encoding: 8bit\\n\"\n";
+  cout << "\n";
+
+  // TODO: msgid is a unique key, and some duplicate (Command-only?) scenes
+  // exist
+  for (std::size_t i = 0; i < scxfile.scene_count(); ++i) {
+    const auto& scene = scxfile.scene(i);
+    if (scene.text.empty()) {
+      continue;
+    }
+    // TODO: Skip scenes that contain only commands.
+    // TODO: Split dialog lines at commands? Otherwise, need to escape the
+    // commands since the \n and \r commands raise warnings in Poedit
+    cout << "#. Chapter " << scene.chapter << " Scene " << scene.scene << "\n";
+    cout << "#: Scene(" << i << ").text\n";
+    cout << "msgid \"" << scene.text << "\"\n";
+    cout << "msgstr \"\"\n\n";
+  }
+  return 0;
+}
---
+++ gettext_converter/export_variables.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using std::cerr;
+using std::cout;
+#include <string>
+using std::string;
+
+#include "scx.hpp"
+
+int main(void) {
+  SCXFile scxfile;
+
+  const auto sourceFile = string{"../../avking.scx"};
+
+  if (!scxfile.read(sourceFile)) {
+    cerr << "Failed to read source: " << sourceFile << "\n";
+    return 1;
+  }
+
+  /*
+   * PO File format:
+   * https://www.gnu.org/software/gettext/manual/gettext.html#PO-Files
+   */
+
+  cout << "# AVKing Scenario Translation\n";
+  cout << "msgid \"\"\n";
+  cout << "msgstr \"\"\n";
+  cout << "\"Language: ja\\n\"\n";
+  cout << "\"Content-Type: text/plain; charset=utf-8\\n\"\n";
+  cout << "\"Content-Transfer-Encoding: 8bit\\n\"\n";
+  cout << "\n";
+
+  // TODO: msgid is a unique key, and some duplicate comments exist.
+  for (std::size_t i = 0; i < scxfile.variable_count(); ++i) {
+    const auto& variable = scxfile.variable(i);
+    cout << "#: Variable(" << i << ").name\n";
+    cout << "msgid \"" << variable.name << "\"\n";
+    cout << "msgstr \"\"\n\n";
+    if (!variable.comment.empty()) {
+      cout << "#: Variable(" << i << ").comment\n";
+      cout << "msgid \"" << variable.comment << "\"\n";
+      cout << "msgstr \"\"\n\n";
+    }
+  }
+  return 0;
+}
